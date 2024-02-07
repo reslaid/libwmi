1780061a1ac8f2857aac683e8ceec47c0a9c18d0
Hayes <154455366+reslaid@users.noreply.github.com>
2024-02-07 11:47:10 +0200

    Update wmi.hpp
    + Added documentation in header file

--- wmi.hpp
+++ wmi.hpp
@@ -11,28 +11,80 @@
 
 #pragma endregion
 
-  class WmiLib
-  {
-  public:
-      WmiLib();
-      ~WmiLib();
-  
-      bool connect();
-      void disconnect();
-  
-      bool execQuery(const std::wstring& query);
-      bool next();
-  
-      std::wstring getString(const std::wstring& propName);
-      int getInt(const std::wstring& propName);
-      double getDouble(const std::wstring& propName);
-      bool getBool(const std::wstring& propName);
-  
-  private:
-      IWbemLocator* pLoc;
-      IWbemServices* pSvc;
-      IEnumWbemClassObject* pEnumerator;
-      IWbemClassObject* pclsObj;
-  };
+/**
+* @brief Class for working with Windows Management Instrumentation (WMI)
+*/
+class WmiLib
+{
+public:
+    /**
+    * @brief Constructor for the WmiLib class
+    */
+    WmiLib();
+
+    /**
+    * @brief WmiLib class destructor
+    */
+    ~WmiLib();
+
+    /**
+    * @brief Establishes a connection to WMI
+    * @return true if the connection was successful, false otherwise
+    */
+    bool connect();
+
+    /**
+    * @brief Closes the WMI connection
+    */
+    void disconnect();
+
+    /**
+    * @brief Performs a WMI query
+    * @param query WMI query in WQL format
+    * @return true if the request was successful, false otherwise
+    */
+    bool execQuery(const std::wstring& query);
+
+    /**
+    * @brief Moves to the next query result object
+    * @return true if the transition was successful, false otherwise
+    */
+    bool next();
+
+    /**
+    * @brief Gets the string value of a WMI object property
+    * @param propName Property name
+    * @return String value of the property
+    */
+    std::wstring getString(const std::wstring& propName);
+
+    /**
+     * @brief Gets the integer value of a WMI object property
+     * @param propName Property name
+     * @return The integer value of the property
+     */
+    int getInt(const std::wstring& propName);
+
+    /**
+    * @brief Gets the floating point value of a WMI object property
+    * @param propName Property name
+    * @return The float value of the property
+    */
+    double getDouble(const std::wstring& propName);
+
+    /**
+    * @brief Gets the boolean value of a WMI object property
+    * @param propName Property name
+    * @return Boolean property value
+    */
+    bool getBool(const std::wstring& propName);
+
+
+private:
+    IWbemLocator* pLoc; ///< Pointer to IWbemLocator
+    IWbemServices* pSvc; ///< Pointer to IWbemServices
+    IEnumWbemClassObject* pEnumerator; ///< Pointer to IEnumWbemClassObject
+    IWbemClassObject* pclsObj; ///< Pointer to IWbemClassObject
+};
 
 #endif
